82c2e091e3412b7636e076ed8995bcb9a41b32b8
Yash <141565294+Yashz74@users.noreply.github.com>
2023-09-29 23:15:31 +0530

    aaa

--- sketch.js
+++ sketch.js
@@ -1,11 +1,8 @@
 let capture;
 let posenet;
-let noseX,noseY;
-let reyeX,reyeY;
-let leyeX,leyeY;
-let singlePose,skeleton;
+let singlePose, skeleton;
 let actor_img;
-let specs,smoke;
+let specs, smoke;
 
 function setup() {
     createCanvas(800, 500);
@@ -13,18 +10,15 @@ function setup() {
     capture.hide();
 
     posenet = ml5.poseNet(capture, modelLoaded);
-    posenet.on('pose',receivedPoses);
+    posenet.on('pose', receivedPoses);
 
     actor_img = loadImage('images/shahrukh.png');
     specs = loadImage('images/spects.png');
     smoke = loadImage('images/cigar.png');
-
 }
 
-function receivedPoses(poses){
-    console.log(poses);
-
-    if(poses.length > 0){
+function receivedPoses(poses) {
+    if (poses.length > 0) {
         singlePose = poses[0].pose;
         skeleton = poses[0].skeleton;
     }
@@ -35,27 +29,102 @@ function modelLoaded() {
 }
 
 function draw() {
-
-    // images and videos(webcam)
+    // Display video feed
     image(capture, 0, 0);
-    fill(255,0,0);
 
-    if(singlePose){
-        for(let i=0; i<singlePose.keypoints.length; i++){
-            ellipse(singlePose.keypoints[i].position.x, singlePose.keypoints[i].position.y,20);
+    if (singlePose) {
+        // Iterate through keypoints and draw circles
+        for (let i = 0; i < singlePose.keypoints.length; i++) {
+            const keypoint = singlePose.keypoints[i];
+            if (keypoint.score > 0.2) { // Filter keypoints with low confidence
+                fill(255, 0, 0);
+                ellipse(keypoint.position.x, keypoint.position.y, 20);
+            }
         }
-        stroke(255,255,255);
+
+        // Draw skeleton lines
+        stroke(255, 255, 255);
         strokeWeight(5);
-        for(let j=0; j<skeleton.length; j++){
-            line(skeleton[j][0].position.x, skeleton[j][0].position.y, skeleton[j][1].position.x, skeleton[j][1].position.y)
+        for (let j = 0; j < skeleton.length; j++) {
+            const start = skeleton[j][0].position;
+            const end = skeleton[j][1].position;
+            line(start.x, start.y, end.x, end.y);
         }
 
-        //image(specs,singlePose.nose.x-35,singlePose.nose.y-50,80,80);
-        //image(smoke,singlePose.nose.x-35,singlePose.nose.y+10,40,40);
+        // Display images on specific keypoints (e.g., nose)
+        if (singlePose.keypoints[0].score > 0.2) { // Check if nose keypoint is detected
+            image(specs, singlePose.keypoints[0].position.x - 35, singlePose.keypoints[0].position.y - 50, 80, 80);
+            image(smoke, singlePose.keypoints[0].position.x - 35, singlePose.keypoints[0].position.y + 10, 40, 40);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+// let capture;
+// let posenet;
+// let noseX,noseY;
+// let reyeX,reyeY;
+// let leyeX,leyeY;
+// let singlePose,skeleton;
+// let actor_img;
+// let specs,smoke;
+
+// function setup() {
+//     createCanvas(800, 500);
+//     capture = createCapture(VIDEO)
+//     capture.hide();
+
+//     posenet = ml5.poseNet(capture, modelLoaded);
+//     posenet.on('pose',receivedPoses);
+
+//     actor_img = loadImage('images/shahrukh.png');
+//     specs = loadImage('images/spects.png');
+//     smoke = loadImage('images/cigar.png');
+
+// }
+
+// function receivedPoses(poses){
+//     console.log(poses);
+
+//     if(poses.length > 0){
+//         singlePose = poses[0].pose;
+//         skeleton = poses[0].skeleton;
+//     }
+// }
+
+// function modelLoaded() {
+//     console.log('Model has loaded');
+// }
+
+// function draw() {
+
+//     // images and videos(webcam)
+//     image(capture, 0, 0);
+//     fill(255,0,0);
+
+//     if(singlePose){
+//         for(let i=0; i<singlePose.keypoints.length; i++){
+//             ellipse(singlePose.keypoints[i].position.x, singlePose.keypoints[i].position.y,20);
+//         }
+//         stroke(255,255,255);
+//         strokeWeight(5);
+//         for(let j=0; j<skeleton.length; j++){
+//             line(skeleton[j][0].position.x, skeleton[j][0].position.y, skeleton[j][1].position.x, skeleton[j][1].position.y)
+//         }
+
+//         //image(specs,singlePose.nose.x-35,singlePose.nose.y-50,80,80);
+//         //image(smoke,singlePose.nose.x-35,singlePose.nose.y+10,40,40);
 
         
-    }
+//     }
 
     
 
-}
+// }
